553223fb51c62da741274c3ae41fe94206dfad31
RajatSharmaDev <rajatsharma1201@gmail.com>
2020-10-13 10:11:23 +0530

    first commit

---
+++ vendor/bootstrap/js/main.js
@@ -0,0 +1,60 @@
+// // Example starter JavaScript for disabling form submissions if there are invalid fields
+// (function () {
+//   "use strict";
+//   window.addEventListener(
+//     "load",
+//     function () {
+//       // Fetch all the forms we want to apply custom Bootstrap validation styles to
+//       var forms = document.getElementsByClassName("needs-validation");
+//       // Loop over them and prevent submission
+//       var validation = Array.prototype.filter.call(forms, function (form) {
+//         form.addEventListener(
+//           "submit",
+//           function (event) {
+//             if (form.checkValidity() === false) {
+//               event.preventDefault();
+//               event.stopPropagation();
+//             }
+//             form.classList.add("was-validated");
+//           },
+//           false
+//         );
+//       });
+//     },
+//     false
+//   );
+// })();
+
+$(window).scroll(function () {
+  $(".navbar").toggleClass("scrolled", $(this).scrollTop() > 50);
+});
+
+$(function () {
+  let $register = $(".registerForm");
+  if ($register.length) {
+    $register.validate({
+      rules: {
+        mail: {
+          required: true,
+        },
+        subject: {
+          required: true,
+        },
+        description: {
+          required: true,
+        },
+      },
+      messages: {
+        mail: {
+          required: "looks good ",
+        },
+        subject: {
+          required: "looks good",
+        },
+        description: {
+          required: "looks good",
+        },
+      },
+    });
+  }
+});
