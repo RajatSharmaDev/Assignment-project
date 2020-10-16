bd0344b64ba5e0223e13c25e5ac3eb87aeea8b26
Abhishek Bhanot <abhishekbhanot5991@gmail.com>
2020-10-16 17:00:31 +0530

    gsap

--- vendor/bootstrap/js/main.js
+++ vendor/bootstrap/js/main.js
@@ -25,6 +25,117 @@
 //   );
 // })();
 
+gsap.from(".banner-text h3", {duration: 1, x: -200, ease: "bounce"});
+gsap.to(".banner-text h3", {duration: 1, x: 0, ease: "bounce"});
+gsap.from(".why-us img", {
+  scrollTrigger:{
+    trigger: '.why-us img',
+    
+    pin: true,   // pin the trigger element while active
+        start: "center center", // when the top of the trigger hits the top of the viewport
+        end: "center center", // end after scrolling 500px beyond the start
+        scrub: 1, 
+        ease: "bounce"
+  },
+  duration: 0,
+    opacity: 0.5, 
+  
+   });
+gsap.to(".why-us img", {
+scrollTrigger:{
+  trigger: '.why-us img',
+  
+  pin: true,   // pin the trigger element while active
+      start: "center center", // when the top of the trigger hits the top of the viewport
+      end: "center center", // end after scrolling 500px beyond the start
+      scrub: 1, 
+},
+duration: 5,
+  opacity: 0.5, 
+  ease: "bounce"
+ });
+
+ let tc = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".pv-txt h4",
+    start: "top center",
+    toggleActions: "restart none none reset",
+  },
+});
+tc.from(
+  ".pv-txt h4",
+  {
+    duration: 2.5,
+    ease: "bounce",
+    y:20
+  },
+ 
+).to(
+  ".pv-txt h4",
+  {
+    y: 0,
+    duration: 1,
+  },
+);
+//about gsap
+
+gsap.from(".pos-card-gsap-1", {
+  opacity: 0, 
+  x: -100, 
+  duration: 1
+});
+gsap.from(".pos-card-gsap-2", {
+  opacity: 0, 
+  x: 100, 
+  duration: 1
+});
+gsap.from(".pos-card-gsap-3", {
+  opacity: 0, 
+  x: -100, 
+  duration: 1
+});
+gsap.from(".service-txt-anim1", {
+  opacity: 0, 
+  x: 200, 
+  duration: 1
+});
+gsap.from(".service-txt-anim2", {
+  opacity: 0, 
+  x: -200, 
+  duration: 1
+});
+gsap.from(".service-txt-anim3", {
+  opacity: 0, 
+  x: 200, 
+  duration: 1
+});
+
+//  gsap.from(".pv-txt h4", { 
+//   scrollTrigger:{
+//     trigger: ".pv-txt h4",
+//     pin: true,   // pin the trigger element while active
+//     start: "top 50%", // when the top of the trigger hits the top of the viewport
+//     end: "top center",
+//     toggleActions:"restart none reset none"
+//   }, 
+//   duration: 2.5,
+//   ease: "elastic.out(1, 0.3)",
+//   y:20
+//   });
+
+//  gsap.to(".pv-txt h4", { 
+//   scrollTrigger:{
+//     trigger: ".pv-txt h4",
+//     pin: true,   // pin the trigger element while active
+//     start: "top center", // when the top of the trigger hits the top of the viewport
+//     end: "top center",
+//     toggleActions:"restart none none restart"
+//   },  
+//   duration: 2.5,
+//   ease: "elastic.out(1, 0.3)",
+//   y: 0
+//   });
+
 $(window).scroll(function () {
   $(".navbar").toggleClass("scrolled", $(this).scrollTop() > 50);
 });
@@ -58,3 +169,7 @@ $(function () {
     });
   }
 });
+
+
+
+ 
